8759eccf655e904f85a286c0f05aae92606abd7b
Dhi Aurrahman <dio@tetrate.io>
2022-01-31 19:09:19 +0700

    Add ext_authz example (#9)
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

---
+++ examples/ext_authz/main.go
@@ -0,0 +1,63 @@
+// Copyright 2022 Dhi Aurrahman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	_ "embed" // to allow embedding files.
+	"fmt"
+	"os"
+
+	"github.com/tetratelabs/run"
+	runsignal "github.com/tetratelabs/run/pkg/signal"
+	"github.com/tetratelabs/telemetry"
+	"google.golang.org/protobuf/encoding/protojson"
+	"sigs.k8s.io/yaml"
+
+	extAuthz "github.com/dio/rundown/api/ext_authz"
+	"github.com/dio/rundown/generated/authservice/config"
+)
+
+//go:embed ext_authz.yaml
+var configYAML []byte
+
+func main() {
+	var (
+		logger = telemetry.NoopLogger()
+		g      = &run.Group{Name: "example", Logger: logger}
+		proxy  = extAuthz.New(g, &extAuthz.Config{
+			Logger:         g.Logger,
+			GenerateConfig: generate,
+		})
+		signalHandler = new(runsignal.Handler)
+	)
+	g.Register(proxy, signalHandler)
+	if err := g.Run(); err != nil {
+		fmt.Printf("program exit: %+v\n", err)
+		os.Exit(1)
+	}
+}
+
+func generate() (*config.Config, error) {
+	j, err := yaml.YAMLToJSON(configYAML)
+	if err != nil {
+		return nil, err
+	}
+	var config config.Config
+	err = protojson.Unmarshal(j, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
